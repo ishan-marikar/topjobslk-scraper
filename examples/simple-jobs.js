1b29f404b53c709e0d198e4482be87b18f8036fc
Ishan Marikar <ishan@imarikar.com>
2017-08-07 15:10:48 +0530

    Added example in Readme

--- examples/simple-jobs.js
+++ examples/simple-jobs.js
@@ -1,18 +1,11 @@
-const TopJobs = require('../');
+const TopJobs = require("../");
 let topJobs = new TopJobs();
 
 topJobs
   .scrapePage(TopJobs.Section.SOFTWARE)
-  .then((data) => {
+  .then(data => {
     console.log(JSON.stringify(data, null, 2));
   })
-  .catch((error) => {
-    console.log(error);
-  });
-
-topJobs
-  .getPages(9).then((data) => {
-    console.log(JSON.stringify(data, null, 2));
-  }).catch((error) => {
+  .catch(error => {
     console.log(error);
   });
